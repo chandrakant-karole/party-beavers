2aaaadbfff76ed0f0af6a6ac6cf26374e54cf607
chandrakant-karole <chandrakantkarole001995@gmail.com>
2021-10-12 14:40:23 +0530

    revert navbar

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -35,28 +35,6 @@ function Header() {
                     </Col>
                     <Col lg={2}></Col>
                 </Row>
-                <Row>
-                    <Navbar className="nav_main_div" bg="light" expand="lg">
-                        <Navbar.Brand><NavLink className="navbar-brand" to="/">Party Beavers HQ</NavLink></Navbar.Brand>
-                        <Navbar.Toggle aria-controls="navbarScroll" />
-                        <Navbar.Collapse id="navbarScroll" className="nav_container">
-                            <Nav
-                                className="mr-auto my-2 my-lg-0 navlist_div"
-                                style={{ maxHeight: '300px' }}
-                                navbarScroll
-                            >
-                                <NavLink className="nav-link" to="/">Home</NavLink>
-                                <NavLink className="nav-link" to="/about">About</NavLink>
-                                <NavDropdown title="Portfolio" id="navbarScrollingDropdown">
-                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio">Portfolio</NavLink></NavDropdown.Item>
-                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio1">Portfolio1</NavLink></NavDropdown.Item>
-                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio2">Portfolio2</NavLink></NavDropdown.Item>
-                                </NavDropdown>
-                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </Row>
             </Container>
         </>
     )
