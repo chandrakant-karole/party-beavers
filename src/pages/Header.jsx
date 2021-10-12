ecfb059a48b75b62f06ac9f61189aee659cb7999
chandrakant-karole <chandrakantkarole001995@gmail.com>
2021-10-12 14:22:14 +0530

    navbar in jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Navbar, NavLink, Nav, NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import Logo from '../assets/images/Logo (2).png';
@@ -34,6 +34,28 @@ function Header() {
                         </div>
                     </Col>
                     <Col lg={2}></Col>
+                </Row>
+                <Row>
+                    <Navbar className="nav_main_div" bg="light" expand="lg">
+                        <Navbar.Brand><NavLink className="navbar-brand" to="/">Party Beavers HQ</NavLink></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll" className="nav_container">
+                            <Nav
+                                className="mr-auto my-2 my-lg-0 navlist_div"
+                                style={{ maxHeight: '300px' }}
+                                navbarScroll
+                            >
+                                <NavLink className="nav-link" to="/">Home</NavLink>
+                                <NavLink className="nav-link" to="/about">About</NavLink>
+                                <NavDropdown title="Portfolio" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio">Portfolio</NavLink></NavDropdown.Item>
+                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio1">Portfolio1</NavLink></NavDropdown.Item>
+                                    <NavDropdown.Item><NavLink className="nav-link" to="/portfolio2">Portfolio2</NavLink></NavDropdown.Item>
+                                </NavDropdown>
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
                 </Row>
             </Container>
         </>
